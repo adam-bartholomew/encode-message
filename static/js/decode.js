586d79bd750d3fc5b36fb62822d300f5c36ba8e6
adam-bartholomew <adam.bartholomew17@gmail.com>
2023-05-26 17:17:04 -0400

    Try and fix vercel build #8

--- static/js/decode.js
+++
@@ -1,9 +0,0 @@
-function clearDecodeForm() {
-    document.getElementById('inputMessage').value = "";
-    document.getElementById('decodeOffset').value = "";
-    document.getElementById('decodedMessage').value = "";
-}
-
-function clearDecodeInput() {
-    document.getElementById('inputMessage').value = "";
-}
--- static/js/encode.js
+++
@@ -1,9 +0,0 @@
-function clearEncodeForm() {
-    document.getElementById('inputMessage').value = "";
-    document.getElementById('encodeOffset').value = "";
-    document.getElementById('encodedMessage').value = "";
-}
-
-function clearEncodeInput() {
-    document.getElementById('inputMessage').value = "";
-}
