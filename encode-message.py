10b1dbbbdcc944fc7f14543c37604d1176d257e7
adam-bartholomew <adam.bartholomew17@gmail.com>
2023-07-05 15:12:03 -0400

    add authentication pages and sql alchemy.

--- encode-message.py
+++ encode-message.py
@@ -1,12 +1,54 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
+from flask_login import LoginManager, login_user, UserMixin, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 import messaging as messaging
 import os
 import func_timeout
 
+# Create flask application
 app = Flask(__name__)
 app.secret_key = os.environ.get('FLASK_KEY')  # FLASK_KEY stored as system environment variable.
 app.debug = True
+
+# Tells flask-sqlalchemy what database to connect to
+db_basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(db_basedir, 'encodeMessage.db')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize flask-sqlalchemy extension
+db = SQLAlchemy()
+
 return_spacer = "-----------------------"
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(25), unique=True, nullable=False)
+    password = db.Column(db.String(50), unique=True, nullable=False)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    email = db.Column(db.String(80), unique=True)
+    #creation_datetime = db.Column(db.Datetime(timezone=False), nullable=False, server_default=func.now())
+    #last_modified_datetime = db.Column(db.Datetime(timezone=False))
+    #creation_userid = db.Column(db.Integer, nullable=False, server_default='admin')
+    #last_modified_userid = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<User {self.user_id}>"
+
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
+# Creates a user loader callback that returns the user object given an id
+@login_manager.user_loader
+def loader_user(user_id):
+    return User.query.get(user_id)
 
 
 # Load Browser Favorite Icon
@@ -31,6 +73,12 @@ def add_header(response):
 @app.route('/')
 def index():
     template = 'index.html'
+    return render_template(template)
+
+
+@app.route('/home')
+def home():
+    template = 'home.html'
     return render_template(template)
 
 
@@ -96,3 +144,44 @@ def decode():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/profile')
+def profile():
+    return render_template('user_profile.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        user = User(user_id=request.form.get("username"), password=request.form.get("password"))
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("login"))
+    return render_template("sign_up.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # If a post request was made, find the user by filtering for the username
+    if request.method == 'POST':
+        user = User.query.filter_by(
+            user_id=request.form.get("username")).first()
+        # Check if the password entered is the same as the user's password
+        if user.password == request.form.get("password"):
+            # Use the login_user method to log in the user
+            login_user(user)
+            return redirect(url_for("home"))
+        # Redirect the user back to the home
+        # (we'll create the home route in a moment)
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    app.run()
